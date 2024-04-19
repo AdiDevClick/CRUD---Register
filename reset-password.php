941d97c7a2ec43aea1057b52dff95b5b8f257e2e
AdiDevClick <adrienquijo@gmail.com>
2024-04-19 19:26:59 +0200

    recovery password html fix

--- reset-password.php
+++ reset-password.php
@@ -4,19 +4,30 @@
 ?>
 <main>
     <section class="wrapper-main">
-        <div class="pw-recovery">
+        <div class="contact-grid">
+            <div class="contact-img">
+                <img src="https://booking.webestica.com/assets/images/element/signin.svg" alt="" srcset="">
+            </div>
             <div class="card">
                 <!-- <div class="section-default"> -->
                 <div class="card-header">
-                    <h3 class="contact-section">Vous avez oublié votre mot de passe ?</h3>
+                    <h3 class="contact-section">Mot de passe oublié ?</h3>
                 </div>
                 <!-- <h1>Réinitialiser votre mot de passe</h1> -->
-                <p class="contact-section">Saisissez l'adresse e-mail associé à votre compte.</p>
+                <div>
+                    <p class="contact-section">Saisissez l'adresse e-mail associée à votre compte.</p>
+                </div>
                 <!-- <p>Un email vous sera envoyé avec les instructions pour réinitialiser votre mot de passe.</p>                 -->
-                <form class="form-index" action="includes/reset-request.inc.php" method="post">
+                <form class="form-contact js-form" action="includes/reset-request.inc.php" method="post">
                     <!-- <label for="email">Votre Email</label> -->
-                    <input class="input" type="text" id="email" name="email" placeholder="Entrez votre adresse email...">
-                    <button class="btn" type="submit" name="submit">Recevoir un nouveau mot de passe</button>
+                    <!-- Email -->
+                    <div class="form">
+                        <input class="input" type="text" id="email" name="email" placeholder="Entrez votre adresse email...">
+                    </div>
+                    <!-- Submit Button -->
+                    <div class="form">
+                        <button class="btn" type="submit" name="submit">Recevoir un nouveau mot de passe</button>
+                    </div>
                 </form>
                 <?php
                     if (isset($_GET["reset"])) {
