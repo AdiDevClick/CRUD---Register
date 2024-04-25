6361d6032ac2db13ca20eda40843f17f4d148cdc
Adrien <adrienquijo@gmail.com>
2024-04-25 14:05:15 +0200

    CSS improvement dans la page de création de recette

--- recipes/submit_recipes.php
+++ recipes/submit_recipes.php
@@ -70,74 +70,94 @@ $errorMessage = CheckInput::showErrorMessage();
             <div class="contact-grid" id="recipe_creation">
             <!-- <div class="form-flex " id="recipe_creation"> -->
                 <!-- <h1>Partagez votre recette !</h1> -->
-                <div class="form-recipe">
+                <div class="card form-recipe">
                     <form action="create_recipes.php" method="post">
-                        <label for="title" class="label">Titre de votre recette</label>
-                        <input class="form" name="title" type="text" id="title" placeholder="Votre titre..." class="input">
-
-                        <label for="step_1" class="label">Votre première étape</label>
-                        <textarea class="" name="step_1" id="step_1" cols="60" rows="3" placeholder="Renseignez votre première étape..."></textarea>
-                        
-                        <label for="step_2" class="label">Votre deuxième étape</label>
-                        <textarea name="step_2" id="step_2" cols="60" rows="3" placeholder="Renseignez  votre deuxième étape..."></textarea>
-                        
-                        <label for="step_3" class="label">Votre troisième étape</label>
-                        <textarea name="step_3" id="step_3" cols="60" rows="3" placeholder="Renseignez  votre troisième étape..."></textarea>
-                        
-                        <label for="step_4" class="label">Votre quatrième étape</label>
-                        <textarea name="step_4" id="step_4" cols="60" rows="3" placeholder="Renseignez  votre quatrième étape..."></textarea>
-                        
-                        <label for="step_5" class="label">Votre cinquième étape</label>
-                        <textarea name="step_5" id="step_5" cols="60" rows="3" placeholder="Renseignez  votre cinquième étape..."></textarea>
-                        
-                        <label for="step_6" class="label">Votre sixième étape</label>
-                        <textarea name="step_6" id="step_6" cols="60" rows="3" placeholder="Renseignez  votre sixième étape..."></textarea>
-
-                        <button type="submit" name="submit" class="btn">Partagez votre recette</button>
-                    <!-- </form> -->
+                        <!-- Title -->
+                        <div class="">
+                            <label for="title" class="label">Titre de votre recette</label>
+                            <input class="form" name="title" type="text" id="title" placeholder="Votre titre..." class="input">
+                        </div>
+                        <!-- STEP 1 -->
+                        <div class="">
+                            <label for="step_1" class="label">Etape 1</label>
+                            <textarea class="" name="step_1" id="step_1" cols="60" rows="3" placeholder="Renseignez votre première étape..."></textarea>
+                        </div>
+                        <!-- STEP 2 -->
+                        <div class="">
+                            <label for="step_2" class="label">Etape 2</label>
+                            <textarea name="step_2" id="step_2" cols="60" rows="3" placeholder="Renseignez  votre deuxième étape..."></textarea>
+                        </div>
+                        <!-- STEP 3 -->
+                        <div class="">
+                            <label for="step_3" class="label">Etape 3</label>
+                            <textarea name="step_3" id="step_3" cols="60" rows="3" placeholder="Renseignez  votre troisième étape..."></textarea>
+                        </div>
+                        <!-- STEP 4 -->
+                        <div class="">
+                            <label for="step_4" class="label">Etape 4</label>
+                            <textarea name="step_4" id="step_4" cols="60" rows="3" placeholder="Renseignez  votre quatrième étape..."></textarea>
+                        </div>
+                        <!-- STEP 5 -->
+                        <div class="">
+                            <label for="step_5" class="label">Etape 5</label>
+                            <textarea name="step_5" id="step_5" cols="60" rows="3" placeholder="Renseignez  votre cinquième étape..."></textarea>
+                        </div>
+                        <!-- STEP 6 -->
+                        <div class="">
+                            <label for="step_6" class="label">Etape 6</label>
+                            <textarea name="step_6" id="step_6" cols="60" rows="3" placeholder="Renseignez  votre sixième étape..."></textarea>
+                        </div>
+                        <!-- Submit -->
+                        <div id="register-btn" class="form form-hidden">
+                            <button type="submit" name="submit" class="btn">Partagez votre recette</button>
+                        </div>
+                        <!-- </form> -->
                 </div>
-                <div class="recipe">
+                <div class="card recipe hidden">
                     <!-- <div class="preparation_header"> -->
                     <!-- DEBUT DE LA CARTE PREPARATION -->
-                    <div class="card-header-section">
-                        <div class=" icon form-logo">
-                            <img src="../img/cooking.svg" alt="icône représentant une cuisson" srcset="">
+                    <div>
+                        <div class="card-header-section">
+                            <div class=" icon form-logo">
+                                <img src="../img/cooking.svg" alt="icône représentant une cuisson" srcset="">
+                            </div>
+                            <div class="card-header title">
+                                <h3 class="contact-section header">Préparation</h3>
+                            </div>
                         </div>
-                        <div class="card-header title">
-                            <h3 class="contact-section header">Préparation</h3>
+                        <div class="total_time">
+                            <div class="time">
+                                <label for="time" class="label">Temps total</label>
+                                <input id="time" type="text" name="time" class="input"></label>
+                                <select class="select" name="select_time" id="select_time" aria-placeholder="test">
+                                    <option value="empty">-- Temps --</option>
+                                    <option value="min">min</option>
+                                    <option value="heures">heures</option>
+                                </select>
+                            </div>
+                            <div class="time">
+                            <!-- <div class="resting_time"> -->
+                                <label for="resting_time" class="label">Temps de repos</label>
+                                <input id="resting_time" type="text" name="resting_time" class="input"></label>
+                                <select class="select" name="select_time" id="select_time_resting_time" aria-placeholder="test">
+                                    <option value="empty">-- Temps --</option>
+                                    <option value="min">min</option>
+                                    <option value="heures">heures</option>
+                                </select>
+                            </div>
+                            <div class="time">
+                            <!-- <div class="oven"> -->
+                                <label for="oven" class="label">Temps de cuisson</label>
+                                <input id="oven" type="text" name="oven" class="input"></label>
+                                <select class="select" name="select_time" id="select_time_oven" aria-placeholder="test">
+                                    <option value="empty">-- Temps --</option>
+                                    <option value="min">min</option>
+                                    <option value="heures">heures</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                    <div class="total_time">
-                        <div class="time">
-                            <label for="time" class="label">Temps total</label>
-                            <input id="time" type="text" name="time" class="input"></label>
-                            <select class="select" name="select_time" id="select_time" aria-placeholder="test">
-                                <option value="empty">-- Temps --</option>
-                                <option value="min">min</option>
-                                <option value="heures">heures</option>
-                            </select>
-                        </div>
-                        <div class="time">
-                        <!-- <div class="resting_time"> -->
-                            <label for="resting_time" class="label">Temps de repos</label>
-                            <input id="resting_time" type="text" name="resting_time" class="input"></label>
-                            <select class="select" name="select_time" id="select_time_resting_time" aria-placeholder="test">
-                                <option value="empty">-- Temps --</option>
-                                <option value="min">min</option>
-                                <option value="heures">heures</option>
-                            </select>
-                        </div>
-                        <div class="time">
-                        <!-- <div class="oven"> -->
-                            <label for="oven" class="label">Temps de cuisson</label>
-                            <input id="oven" type="text" name="oven" class="input"></label>
-                            <select class="select" name="select_time" id="select_time_oven" aria-placeholder="test">
-                                <option value="empty">-- Temps --</option>
-                                <option value="min">min</option>
-                                <option value="heures">heures</option>
-                            </select>
-                        </div>
-                    </div>
+                    
                     <!-- FIND DE LA CARTE PREPARATION -->
                     <!-- DEBUT DE LA CARTE INGREDIENTS -->
                     <div class="card-header-section">
@@ -149,48 +169,65 @@ $errorMessage = CheckInput::showErrorMessage();
                         </div>
                     </div>
                     <div class="ingredients">
-                        <div class="persons">
+                        <div class="persons time">
                             <label for="persons" class="label">Nombre de personnes</label>
                             <input id="persons" type="text" name="persons" class="input"></label>
                         </div>
-                        <div class="ingredient">
-                            <label for="ingredient" class="label">Vos ingrédients</label>
-                            <select class="select" name="ingredient" id="ingredient" aria-placeholder="test">
-                                <option value="empty">-- Choisissez votre ingrédient --</option>
-                                <option value="oeuf">Oeuf</option>
-                                <option value="sel">Sel</option>
-                                <option value="sucre">Sucre</option>
-                                <option value="beurre">Beurre/option>
-                            </select>
-                            <select class="select" name="ingredient" id="ingredient" aria-placeholder="test">
-                                <option value="empty">-- Choisissez votre ingrédient --</option>
-                                <option value="oeuf">Oeuf</option>
-                                <option value="sel">Sel</option>
-                                <option value="sucre">Sucre</option>
-                                <option value="beurre">Beurre/option>
-                            </select>
-                            <select class="select" name="ingredient" id="ingredient" aria-placeholder="test">
-                                <option value="empty">-- Choisissez votre ingrédient --</option>
-                                <option value="oeuf">Oeuf</option>
-                                <option value="sel">Sel</option>
-                                <option value="sucre">Sucre</option>
-                                <option value="beurre">Beurre/option>
-                            </select>
-                            <select class="select" name="ingredient" id="ingredient" aria-placeholder="test">
+                        <div class="">
+                            <p for="ingredient" class="label">Vos ingrédients</p>
+                            <div class="ingredient">
+                            <select class="select" name="ingredient" id="ingredient1" aria-placeholder="test">
                                 <option value="empty">-- Choisissez votre ingrédient --</option>
                                 <option value="oeuf">Oeuf</option>
                                 <option value="sel">Sel</option>
                                 <option value="sucre">Sucre</option>
                                 <option value="beurre">Beurre</option>
                             </select>
-                            <div class="add_ingredient">
-                                <button name="add_ingredient" id="button" type="button" class="btn">Ajouter un ingrédient</button>
+                            <select class="select" name="ingredient" id="ingredient2" aria-placeholder="test">
+                                <option value="empty">-- Choisissez votre ingrédient --</option>
+                                <option value="oeuf">Oeuf</option>
+                                <option value="sel">Sel</option>
+                                <option value="sucre">Sucre</option>
+                                <option value="beurre">Beurre</option>
+                            </select>
+                            <select class="select" name="ingredient" id="ingredient3" aria-placeholder="test">
+                                <option value="empty">-- Choisissez votre ingrédient --</option>
+                                <option value="oeuf">Oeuf</option>
+                                <option value="sel">Sel</option>
+                                <option value="sucre">Sucre</option>
+                                <option value="beurre">Beurre</option>
+                            </select>
+                            <select class="select" name="ingredient" id="ingredient4" aria-placeholder="test">
+                                <option value="empty">-- Choisissez votre ingrédient --</option>
+                                <option value="oeuf">Oeuf</option>
+                                <option value="sel">Sel</option>
+                                <option value="sucre">Sucre</option>
+                                <option value="beurre">Beurre</option>
+                            </select>
+                            <select class="select" name="ingredient" id="ingredient5" aria-placeholder="test">
+                                <option value="empty">-- Choisissez votre ingrédient --</option>
+                                <option value="oeuf">Oeuf</option>
+                                <option value="sel">Sel</option>
+                                <option value="sucre">Sucre</option>
+                                <option value="beurre">Beurre</option>
+                            </select>
+                            <select class="select" name="ingredient" id="ingredient6" aria-placeholder="test">
+                                <option value="empty">-- Choisissez votre ingrédient --</option>
+                                <option value="oeuf">Oeuf</option>
+                                <option value="sel">Sel</option>
+                                <option value="sucre">Sucre</option>
+                                <option value="beurre">Beurre</option>
+                            </select>
                             </div>
+                            
                             <div class="add_ingredient">
-                                <label for="custom_ingredient" class="label">Votre ingrédient particulier</label>
-                                <input id="custom_ingredient" type="text" name="custom_ingredient" class="input" placeholder="Votre ingrédient..."></input>
-                                <button name="add_custom" id="add_custom" type="button" class="btn">Ajouter</button>
+                                <button name="add_ingredient" id="button" type="button" class="btn">Ajouter un nouvel ingrédient</button>
                             </div>
+                        </div>
+                        <div class="add_ingredient">
+                            <label for="custom_ingredient" class="label">Votre ingrédient particulier</label>
+                            <input id="custom_ingredient" type="text" name="custom_ingredient" class="input" placeholder="Votre ingrédient..."></input>
+                            <button name="add_custom" id="add_custom" type="button" class="btn">Ajouter</button>
                         </div>
                     </div>
                 </div>
