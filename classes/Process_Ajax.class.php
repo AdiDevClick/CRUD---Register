70c185432790aac0d2657b16501dc7e0b293295b
AdiDevClick <adrienquijo@gmail.com>
2024-07-03 18:22:43 +0200

    PHP : Création d'une classe Process_Ajax pour traiter l'échange Ajax - Cela permettra un fichier plus clair et versatile -

---
+++ classes/Process_Ajax.class.php
@@ -0,0 +1,82 @@
+<?php
+
+class Process_Ajax 
+{
+    private bool $isImageSent;
+    private array $getDatas;
+    private bool $isImageAlreadyOnServer = false;
+
+    public function __construct(
+        private array $Post_Data,
+        private array $Post_Files,
+        )
+    {
+        $getDatas = $this::$getDatas;
+        $isImageSent = $this::$isImageSent;
+        $isImageAlreadyOnServer = $this::$isImageAlreadyOnServer;
+
+        if ($this->$Post_Data) {
+            $this->$getDatas = [
+                'title' => $this->$Post_Data['title'],
+                'step_1' => $this->$Post_Data['step_1'],
+                'step_2' => $this->$Post_Data['step_2'],
+                'step_3' => $this->$Post_Data['step_3'],
+                'step_4' => $this->$Post_Data['step_4'],
+                'step_5' => $this->$Post_Data['step_5'],
+                'step_6' => $this->$Post_Data['step_6'],
+                'persons' => $this->$Post_Data['persons'],
+                'total_time' => $this->$Post_Data['total_time'],
+                'total_time_length' => $this->$Post_Data['total_time_length'],
+                'resting_time' => $this->$Post_Data['resting_time'],
+                'resting_time_length' => $this->$Post_Data['resting_time_length'],
+                'oven_time' => $this->$Post_Data['oven_time'],
+                'oven_time_length' => $this->$Post_Data['oven_time_length'],
+                'ingredient' => $this->$Post_Data['ingredient'],
+                'ingredient2' => $this->$Post_Data['ingredient2'],
+                'ingredient3' => $this->$Post_Data['ingredient3'],
+                'ingredient4' => $this->$Post_Data['ingredient4'],
+                'ingredient5' => $this->$Post_Data['ingredient5'],
+                'ingredient6' => $this->$Post_Data['ingredient6'],
+                'custom_ingredients' => $this->$Post_Data['custom_ingredient'],
+                // Envoi toutes les données reçues au format JSON vers le serveur -
+                // Les données ont préalablement été vérifiées en JavaScript mais
+                // il ne faudra pas oublier de sanitize on use -
+                // 'total_time' => $content,
+                // Utiliser json_encode($array) pour permettre
+                // de réencoder en JSON si nécessaire
+                // 'persons' => json_encode($dataTest)
+            ];
+
+            $setRecipe = new RecipeView($getDatas);
+            $recipeId = $setRecipe->insertRecipeTest();
+
+            if ($this->$Post_Files['file'] && $this->$Post_Files['file']['error'] == 0) {
+                // On vérifie que le fichier ne pèse pas plus d'10Mo
+                if ($this->$Post_Files['file']['size'] < 10 * 1024 * 1024) {
+                    $loggedUser = LoginController::checkLoggedStatus();
+                    $fileinfo = pathinfo($this->$Post_Files['file']['name']);
+                    $extension = strtolower($fileinfo['extension']);
+                    // On vérifie les extensions autorisée
+                    $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+                    $new_name = time() . '.' . $extension;
+                    $dir = '../uploads/'. $loggedUser['email'] . '/recipes_images/' . $recipeId;
+                    $file = $dir . '/' . $new_name;
+                    if (in_array($extension, $allowedExtensions)) {
+                        makeDir($dir);
+                        move_uploaded_file($this->$Post_Files['file']['tmp_name'], $file);
+                        $isImageSent = true;
+                        $image_Data = ['recipeId' => $recipeId, 'fileName' => $new_name, 'filePath' => $file];
+                        $setRecipe->insertImage($image_Data);
+                    } else {
+                        $isImageSent = false;
+                        // echo "Votre fichier n'est pas compatible";
+                    }
+                } else {
+                    $isImageAlreadyOnServer = true;
+                    // echo "Déjà envoyé..";
+                }
+                echo json_encode(['status' => 'success', 'img_status' => $isImageSent, 'is_on_server' => $isImageAlreadyOnServer]);
+            }
+        }
+    }
+}
