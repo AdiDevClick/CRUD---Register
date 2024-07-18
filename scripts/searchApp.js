c02c469297959777a8194e8db48ab7a3267e8135
AdiDevClick <adrienquijo@gmail.com>
2024-07-18 19:21:09 +0200

    SearchBar : L'ouverture et la fermeture de la barre de recherche du header est maintenant activée

--- scripts/searchApp.js
+++ scripts/searchApp.js
@@ -1,9 +1,43 @@
 import { SearchBar } from "./components/SearchBar.js"
 
-const searchBar = document.querySelectorAll('#search-form')
+const searchForm = document.querySelectorAll('#search-form')
+const searchBar = document.querySelector('#search')
+const closeSearchBtn = searchBar.querySelector('.close-search')
+const searchIcon = searchBar.querySelector('.icon-search-input')
+const body = document.querySelector('main')
+const navLinks = document.querySelector('.links')
+let isOpened
 
-searchBar.forEach(bar => {
-    new SearchBar(bar, {
+const onClose = function (e) {
+    e.preventDefault()
+    console.log(e)
+    if (isOpened && searchBar.classList.contains('open')) {
+        navLinks.classList.remove('hidden')
+        searchBar.classList.remove('open')
+        isOpened = false
+    }
+}
+
+const onOpen = function (e) {
+    e.preventDefault()
+    console.log(e)
+    if (searchBar.classList.contains('open') || isOpened) return
+    if (!isOpened) {
+        isOpened = true
+        searchBar.classList.add('open')
+        navLinks.classList.add('hidden')
+        searchBar.addEventListener('transitionend', (e) => {
+            body.addEventListener('click', onClose, {once: true})
+            closeSearchBtn.addEventListener('click', onClose, {once: true})
+        }, {once: true})
+    }
+}
+
+searchIcon.addEventListener('click', onOpen)
+
+searchForm.forEach(form => {
+    new SearchBar(form, {
         debounceDelay: 1000
     })
 })
+
