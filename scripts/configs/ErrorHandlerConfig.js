db87a30a204318e02f6bd7a72359d3fce23f450b
Adrien <adrienquijo@gmail.com>
2024-09-21 23:10:29 +0200

    Ajout de quelques configuration pour le handler

--- scripts/configs/ErrorHandlerConfig.js
+++ scripts/configs/ErrorHandlerConfig.js
@@ -1,50 +1,70 @@
-/**
- * ERROR HANDLER CONFIG FILE
- */
 ////////////////////////////////////////////////////////////
-/** MESSAGES */
+////////////////////////////////////////////////////////////
+            /**
+             * ERROR HANDLER CONFIG FILE
+             */
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+                    /** FORM */
 
-// Email error message :
+/** @type {String} Configurate form.id element to avoid checking */
+export const formIDToAvoidChecking = 'search-form'
+
+/** @type {String} Configurate form submit button ID to target and listen */
+export const formButton = '#submit'
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+                    /** MESSAGES */
+
+/**  @type {String} Configurate invalid email error message */
 export const invalidEmailMessage = `Votre email est invalide 
                 exemple valide : monEmail@mail.fr`
 
-// Password invalid message :
+/**  @type {String} Configurate password invalid error message */
 export const invalidPwMessage = 'Vos mots de passes ne sont pas identiques'
 
-// No space allowed message :
+/**  @type {String} Configurate no space allowed error message */
 export const noSpaceAllowedMessage = 'Veuillez ne pas utiliser d\'espace'
 
-// Only integer allowed message :
+/**  @type {String} Configurate only integer allowed error message */
 export const notANumberError = 'Seuls les nombres sont autorisés'
 
-// Empty inputs message
-export const emptyAlert = 'Un ou plusieurs champs sont vides'
+/**  @type {String} Configurate empty inputs error message */
+export const emptyAlert = 'Un ou plusieurs champs doivent être renseignés'
 
-/** MESSAGES END ********/
 ////////////////////////////////////////////////////////////
-/** INPUTS TO LISTEN TO */
+////////////////////////////////////////////////////////////
+                /** INPUTS TO LISTEN TO */
 
+/**  @type {String} Configurate elements to be checked */
 export const inputsToListen = 'input, textarea'
 
-/** INPUTS TO LISTEN TO END ********/
-////////////////////////////////////////////////////////////
-/** REGEX */
+/**  @type {String} Configurate elements that should be INTEGERS only */
+export const thisInputShouldBeInt = '#persons, #total_time, #resting_time, #oven_time'
 
-// Can only use this chars :
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+                    /** REGEX */
+
+/**  @type {RegExp} Configurate which characters are allowed */
 export const allowedSpecialChars = new RegExp('^[\\w\\s,.:;_?\'!\\"*()~&éèêëàâäôöûüùçÀ-]+$')
 
-// Email can only be email@email.co :
+/**  @type {RegExp} Configurate Email : Can only be email@email.co */
 export const emailInputRegex = new RegExp("([a-z0-9A-Z._-]+)@([a-z0-9A-Z_-]+)\\.([a-z\.]{2,6})$")
 
-/** REGEX END ********/
 ////////////////////////////////////////////////////////////
-/** ALERT */
+////////////////////////////////////////////////////////////
+                    /** ALERT */
 
-// Alert class :
+/**  @type {String} Configurate alert element class when created */
 export const alertClass = 'alert-error hidden'
 
-// Input error class :
+/**  @type {String} Configurate input element error class when an error occurs */
 export const inputErrorClass = 'input_error'
 
-/** ALERT END ********/
+/**  @type {String} Configurate hidden class name for alert to be removed / added */
+export const hiddenClass = 'hidden'
+
+////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
