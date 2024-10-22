91cf5b0f0af68e5aa2e5d36aa261ac40e45bdb90
Adrien <adrienquijo@gmail.com>
2024-10-22 23:10:28 +0200

    Création d'une classe qui permettra de créer des boutons de validations pour la création de recette

--- scripts/components/ProgressiveStepButton.js
+++ scripts/components/ProgressiveStepButton.js
@@ -1,8 +1,21 @@
+import { createElement } from "../functions/dom.js"
+
 class ProgressiveStepButton {
     
     #r
     constructor(circumference) {
         this.#r = circumference
+    }
+
+    /**
+     * Permet de créer une div sur le DOM comprenant des dataset
+     * pour target les élements nécessaires à l'alerte
+     */
+    #createNewToasterContainer() {
+        const buttonDivContainer = createElement('div', {
+            class: 'circular-progress-button-container'
+        })
+        document.body.append(buttonDivContainer)
     }
 
     #calculateDasharray(r) {
