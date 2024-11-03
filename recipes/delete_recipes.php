77f8e44e66b3b0c6d2afb4c7a72526827f5bf793
Adrien <adrienquijo@gmail.com>
2024-11-03 14:13:13 +0100

    Sécurisation du script pour éviter qu'une personne n'ayant pas les droits d'accès puisses supprimer la recette - Modification du script pour utiliser la fonction requête SQL dynamique fetchFromRecipesTable() -

--- recipes/delete_recipes.php
+++ recipes/delete_recipes.php
@@ -8,58 +8,74 @@ include_once("../includes/class-autoloader.inc.php");
 include_once('../logs/customErrorHandlers.php');
 
 $serverData = $_SERVER['REQUEST_METHOD'] === 'POST';
-//if ($getDatas = isset($_GET)){
+$loggedUser = LoginController::checkLoggedStatus();
+$sessionName = 'ID_RECIPE';
 
-/***
+/**
  * Grabing URL ID from index page and fetching rows datas
  */
 if(isset($_GET['id']) && is_numeric($_GET['id'])) {
     $getDatas = $_GET['id'];
-    $idDatas = new RecipeView($getDatas);
-    $getTitle = $idDatas->getRecipeId();
-} else {
-    // header('Location: ../index.php?error=no-delete-id');
-    //throw new Exception('La recette est introuvable');
+    $id = new RecipeView($getDatas);
+    // Grab author & title and create session
+    $recipeInfos = $id->retrieveFromRecipesTable(['author', 'title'], $id->getSelfData(), $sessionName);
 }
-
+/**
+ * Lors d'un submit si la recette existe
+ */
 if ($serverData && isset($_POST['submit'])) {
-    $getDatas = $_POST['id'];
-    /*    $datas = new Recipecontroller($postDatas);
-       $get = $datas->getRecipeId($postDatas); */
-    $checkId = new RecipeView($getDatas);
-    $getTitle = $checkId->getRecipeId();
+    $postID = $_POST['id'];
+    // Retrieve session data
+    $sessionID = $_SESSION[$sessionName][$sessionName] ?? null;
+    // Grab author & title
+    if (is_numeric($postID) && (is_numeric($sessionID) && $sessionID == $postID)) {
+        $id = new RecipeView($postID);
+        $recipeInfos = $id->retrieveFromRecipesTable(['author', 'title'], $id->getSelfData(), $sessionName);
+    }
+    // If POST ID && GET ID && SessionID aren't same it will fail
     try {
-        if ($checkId->checkId()) {
-            $checkId->deleteRecipe();
+        if ($recipeInfos['author'] === $loggedUser['email']) {
+            $id->deleteRecipe();
+            unset($_SESSION[$sessionName]);
             header('Location: ../index.php?delete=success');
             exit("Il n'y a malheureusement plus rien à voir !");
         } else {
+            unset($_SESSION[$sessionName]);
             header('Location: ../index.php?delete=error');
             exit("Il n'y a malheureusement plus rien à voir !");
         }
-    } catch (Error $e) {
-        die('Erreur : '. $e->getMessage()) . 'Nous ne pouvons pas supprimer cette recette. ';
+    } catch (\Throwable $th) {
+        unset($_SESSION[$sessionName]);
+        die('Erreur : '. $e->getMessage()) . ' Nous ne pouvons pas supprimer cette recette. ';
     }
 }
-ob_start();
+// If it belongs to the user, display delete button
 
+ob_start();
 ?>
-<?php $title = "We Love Food - Suppression de votre recette"?>
-<section class="container">
-    <div class="form-flex">
-        <?php //if (isset($_GET['title'])) :?>
-        <h1>Suppression de la recette : <?= strip_tags($getTitle['title'])?> ?</h1>
-        <?php //endif?>
-        <div class="form">
-            <form action="delete_recipes.php" method="post">
-                <div class="visually-hidden">
-                    <input type="hidden" class="input" name="id" id="id" value="<?= strip_tags($getDatas)?>"/>
-                </div>
-                <button type="submit" name="submit" class="btn btn-alert">Supprimer définitivement</button>
-            </form>
+<?php if ($recipeInfos['author'] === $loggedUser['email']) : ?>
+    <?php $title = "We Love Food - Suppression de votre recette"?>
+    <section class="container">
+        <div class="form-flex">
+            <?php //if (isset($_GET['title'])) :?>
+            <h1>Suppression de la recette : <?= strip_tags($recipeInfos['title'])?> ?</h1>
+            <?php //endif?>
+            <div class="form">
+                <form action="delete_recipes.php" method="post">
+                    <div class="visually-hidden">
+                        <input type="hidden" class="input" name="id" id="id" value="<?= strip_tags($getDatas)?>"/>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-alert">Supprimer définitivement</button>
+                </form>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+    <?php else : ?>
+        <?php session_destroy()?>
+        <?php header('Location: ../index.php?delete=error')?>
+        <?php exit("Il n'y a malheureusement plus rien à voir !") ?>
+<?php endif ?>
 <?php $content = ob_get_clean()?>
 
-<?php require('../templates/layout.php');
+<?php
+require '../templates/layout.php';
