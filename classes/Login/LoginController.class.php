07db8df733eb81260ee3d415a0e7d89a1795c99c
Adrien <adrienquijo@gmail.com>
2024-11-11 12:49:24 +0100

    Utilisation de la fonction sanitize - Ce vieux code a été remanié pour être plus robuste et plus performant

--- classes/Login/LoginController.class.php
+++ classes/Login/LoginController.class.php
@@ -14,25 +14,39 @@ class LoginController extends Login
      * Vérifie les données reçues et les assainit avant de pouvoir les envoyer
      * à la fonction main (->login)
      *
-     * @return void
+     * @return
      */
-    protected function index(): void
+    protected function index()
     {
+        try {
+            if (!isset($this->getDatas)) {
+                throw new Error("Nous ne pouvons pas tester vos identifiants...");
+            } else {
+                $checkInput = new CheckInput($this->getDatas);
+                $sanitized_Datas = $checkInput->sanitizeData();
 
-        if (!isset($this->getDatas)) {
-            throw new Error("Nous ne pouvons pas tester vos identifiants...");
-        } else {
-            $checkInput = new CheckInput(
-                $this->getDatas
-            );
-            $password = $checkInput->test_input($this->getDatas["password"]);
-            $username = $checkInput->test_input($this->getDatas["username"]);
-            // $validInputs = $checkInput->checkInputs();
-            $checkInput->checkInputs();
-            if (empty($checkInput->getErrorsArray())) {
-                $this->login($password, $username);
+                // $password = $checkInput->test_input($this->getDatas["password"]);
+                // $username = $checkInput->test_input($this->getDatas["username"]);
+                $password = $sanitized_Datas["password"];
+                $username = $sanitized_Datas["username"];
+
+                // die(var_dump($username, $password, $_SESSION));
+                // $validInputs = $checkInput->checkInputs();
+                // $checkInput->checkInputs();
+                // if (empty($checkInput->getErrorsArray())) {
+                if (isset($_SESSION['SANITIZED']) && $_SESSION['SANITIZED'] === true) {
+                    unset($_SESSION['SANITIZED']);
+                    $this->login($password, $username);
+                } else {
+                    throw new Error($_SESSION['SANITIZED'][0]);
+                }
+                return;
             }
-            return;
+        } catch (\Throwable $e) {
+            // null;
+            // die('Erreur : ' . $e->getMessage() . " Une erreur est survenue dans la connexion");
+
+            CheckInput::insertErrorMessageInArray($e->getMessage());
         }
     }
 
@@ -48,78 +62,96 @@ class LoginController extends Login
      */
     protected function login(string $pwd, string $email)
     {
+        // die(var_dump($users));
+        // die(var_dump($user));
+        // echo($this->getUsers($email)['email']);
+        // $userEmail = $this->getUsers($email)[0]['email'] ?? null;
+        // $username = $this->getUsers($email)[0]['full_name'] ?? null;
+        // $userID = $this->getUsers($email)[0]['user_id'] ?? null;
+        // $userEmail = $this->getUsers($email)['email'] ?? null;
+        // $username = $this->getUsers($email)['full_name'] ?? null;
+        // $userID = $this->getUsers($email)['user_id'] ?? null;
 
-        if (empty(CheckInput::getErrorsArray())) {
-            // echo ($this->getUsers($email)['email']);
-            $userEmail = $this->getUsers($email)[0]['email'] ?? null;
-            $username = $this->getUsers($email)[0]['full_name'] ?? null;
-            $userID = $this->getUsers($email)[0]['user_id'] ?? null;
 
-            // $script = <<< JS
-            // include_once("templates/toaster_template.html");
-            // // require_once("scripts/toaster.js");
-            // import { alertMessage } from "./scripts/toaster.js"
-            // function() {
-            //     const alert = alertMessage($errorMessage)
-            //     const alertContainer = document.querySelector('.toast-container')
-            //     alertContainer.insertAdjacentElement(
-            //         'beforeend',
-            //         alert
-            //     )
-            //     console.log('object3')
-            // }
-            // JS;
-            // $script2 = <<< JS
+        // $script = <<< JS
+        // include_once("templates/toaster_template.html");
+        // // require_once("scripts/toaster.js");
+        // import { alertMessage } from "./scripts/toaster.js"
+        // function() {
+        //     const alert = alertMessage($errorMessage)
+        //     const alertContainer = document.querySelector('.toast-container')
+        //     alertContainer.insertAdjacentElement(
+        //         'beforeend',
+        //         alert
+        //     )
+        //     console.log('object3')
+        // }
+        // JS;
+        // $script2 = <<< JS
 
-            //     // import { alertMessage } from "./scripts/toaster.js"
+        //     // import { alertMessage } from "./scripts/toaster.js"
 
-            //     console.log('object4')
-            //     const form = document.querySelector('form')
-            //     form.addEventListener('submit', e => {
-            //         e.preventDefault()
-            //         console.log('object2')
-            //         // $script
-            //         // this.#onSubmit(e.currentTarget)
-            //         form.removeEventListener('submit', e)
-            //     })
+        //     console.log('object4')
+        //     const form = document.querySelector('form')
+        //     form.addEventListener('submit', e => {
+        //         e.preventDefault()
+        //         console.log('object2')
+        //         // $script
+        //         // this.#onSubmit(e.currentTarget)
+        //         form.removeEventListener('submit', e)
+        //     })
 
-            // JS;
+        // JS;
+        try {
 
-            try {
-                if ($this->getPwd($pwd, $email) &&
-                    (($userEmail === $email) ||
-                    ($username === strtolower($email)))) {
-                    // (($this->getUsers($email)[0]['email'] === $email) ||
-                    // ($this->getUsers($email)[0]['full_name'] === $email))) {
-                    //password_verify($pwd, $users[0]['password'])) {
+            if (empty(CheckInput::getErrorsArray())) {
+                $user = $this->getUsers($email);
 
-                    if(session_status() !== PHP_SESSION_ACTIVE || session_status() === PHP_SESSION_NONE) {
-                        session_start();
-                    }
-
-                    $this->setCookies($userEmail, $username, $userID);
-
-                    $_SESSION['USER_NAME'] = $this->getUsers($email)[0]['full_name'];
-                    $_SESSION['USER_ID'] = $this->getUsers($email)[0]['user_id'];
-                    $_SESSION['LOGGED_USER'] = [
-                        $this->getUsers($email)[0]['full_name'],
-                        $this->getUsers($email)[0]['user_id'],
-                        $this->getUsers($email)[0]['email']
-                    ];
-                } else {
-                    // $errorMessage = sprintf(
-                    //     'Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
-                    //     $email,
-                    //     $pwd
-                    // );
-                    throw new Error("LGNGETPW - Mot de passe et/ou Login incorrects");
-                    // throw new Error($errorMessage.header("Location: ".Functions::getUrl()."?error=pwd-or-id-does-not-match"));
-                    // throw new Error($errorMessage);
-                    //header("Location: ".Functions::getUrl()."?error=pwd-does-not-match");
-                }
-            } catch (Error $errorMessage) {
-                CheckInput::insertErrorMessageInArray($errorMessage->getMessage());
+                $userEmail = $user['email'] ?? null;
+                $username = $user['full_name'] ?? null;
+                $userID = $user['user_id'] ?? null;
+            } else {
+                // CheckInput::insertErrorMessageInArray($_SESSION['SANITIZED'][0]);
+                throw new Error("LGNGETPWSNTZ - Mot de passe et/ou Login incorrects");
             }
+            if ($this->getPwd($pwd, $email) &&
+                (($userEmail === $email) ||
+                ($username === strtolower($email)))) {
+                // (($this->getUsers($email)[0]['email'] === $email) ||
+                // ($this->getUsers($email)[0]['full_name'] === $email))) {
+                //password_verify($pwd, $users[0]['password'])) {
+
+                if(session_status() !== PHP_SESSION_ACTIVE || session_status() === PHP_SESSION_NONE) {
+                    session_start();
+                }
+
+                $this->setCookies($userEmail, $username, $userID);
+
+                $_SESSION['USER_NAME'] = $username;
+                $_SESSION['USER_ID'] = $userID;
+                $_SESSION['LOGGED_USER'] = [
+                    $username,
+                    $userID,
+                    $userEmail
+                ];
+                // $_SESSION['USER_NAME'] = $this->getUsers($email)[0]['full_name'];
+                // $_SESSION['USER_ID'] = $this->getUsers($email)[0]['user_id'];
+                // $_SESSION['LOGGED_USER'] = [
+                //     $this->getUsers($email)[0]['full_name'],
+                //     $this->getUsers($email)[0]['user_id'],
+                //     $this->getUsers($email)[0]['email']
+                // ];
+            } else {
+                // $errorMessage = sprintf(
+                //     'Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
+                //     $email,
+                //     $pwd
+                // );
+                throw new Error("LGNGETPW - Mot de passe et/ou Login incorrects");
+            }
+        } catch (Error $errorMessage) {
+            // die("". $errorMessage->getMessage());
+            CheckInput::insertErrorMessageInArray($errorMessage->getMessage());
         }
     }
 
@@ -295,9 +327,10 @@ class LoginController extends Login
             }
             // echo ($loggedUser);
             // print_r($loggedUser);
+            return $loggedUser;
+
         } catch (Error $errorMessage) {
             die("Erreur : ". $errorMessage->getMessage() . header('refresh:5,../index.php?error=not-loggedin'));
         }
-        return $loggedUser;
     }
 }
