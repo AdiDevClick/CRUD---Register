705c25a7ba852de735ed8f841d16179a71ea96ab
Adrien <adrienquijo@gmail.com>
2024-12-21 08:00:44 +0100

    Fix du queryselector qui n'était pas toujours disponible avant la demande onReady

--- scripts/carouselApp.js
+++ scripts/carouselApp.js
@@ -1,11 +1,12 @@
 import { importThisModule } from "./functions/dom.js";
 
 const url = window.location;
+const carousel1 = document.querySelector("#carousel1");
 
 async function onReady(url) {
-    if (url.toString().includes("index.php")) {
+    if (url.toString().includes("index.php") && carousel1) {
         const Carousel = await importThisModule("Carousel");
-        Carousel.create(document.querySelector("#carousel1"), {
+        Carousel.create(carousel1, {
             visibleSlides: 3,
             slidesToScroll: 2,
             automaticScrolling: false,
