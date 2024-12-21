550ace9793dd4f960d10af21170f00562c6367cb
Adrien <adrienquijo@gmail.com>
2024-12-21 19:21:24 +0100

    Fix du lancement de l'app, sur firefox elle avait du mal à se lancer proprement

--- scripts/carouselApp.js
+++ scripts/carouselApp.js
@@ -1,25 +1,34 @@
 import { importThisModule } from "./functions/dom.js";
 
 const url = window.location;
-const carousel1 = document.querySelector("#carousel1");
 
 async function onReady(url) {
-    if (url.toString().includes("index.php") && carousel1) {
-        const Carousel = await importThisModule("Carousel");
-        Carousel.create(carousel1, {
-            visibleSlides: 3,
-            slidesToScroll: 2,
-            automaticScrolling: false,
-            infinite: true,
-            loop: false,
-            pagination: true,
-            // navigation: false,
-        });
+    console.log(url);
+    if (
+        url.toString().includes("index.php") ||
+        url.toString() === "/recettes/"
+    ) {
+        console.log("je suis ready");
+        const carousel1 = document.querySelector("#carousel1");
+        if (carousel1) {
+            const Carousel = await importThisModule("Carousel");
+            Carousel.create(carousel1, {
+                visibleSlides: 3,
+                slidesToScroll: 2,
+                automaticScrolling: false,
+                infinite: true,
+                loop: false,
+                pagination: true,
+                // navigation: false,
+            });
+        }
     }
 }
 
 if (window.readyState !== "loading") {
     onReady(url.pathname);
+    console.log("j'ai call le onready");
 } else {
     window.addEventListener("DOMContentLoaded", onReady);
+    console.log("je suis dans le else");
 }
