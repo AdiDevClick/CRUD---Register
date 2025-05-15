16398e834c985616cfdb08bd4d14a726176ab538
Adrien <adrienquijo@gmail.com>
2025-05-15 10:05:15 +0200

    Ajout d'outputs lors du build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import autoprefixer from "autoprefixer";
-import postcssNesting from "postcss-nesting";
-import eslint from "vite-plugin-eslint";
+import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import postcssNesting from 'postcss-nesting';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
     server: {
         // origin: 'https://http://192.168.1.181:5173',
-        origin: "https://127.0.0.1:80",
+        origin: 'https://127.0.0.1:80',
         // origin: 'https://localhost:5173', // Règle le problème des chemins dans le CSS
         warmup: {
-            clientFiles: ["./src/components/*.vue", "./src/utils/big-utils.js"],
-            ssrFiles: ["./src/server/modules/*.js"],
+            clientFiles: ['./src/components/*.vue', './src/utils/big-utils.js'],
+            ssrFiles: ['./src/server/modules/*.js'],
         },
         // hmr: {
         // host: '192.168.1.181',
@@ -19,21 +19,33 @@ export default defineConfig({
         // host: 'vite.adi',
         // }
     },
-    base: "/assets",
+    base: '/assets/',
     build: {
         manifest: true,
         copyPublicDir: false,
-        outDir: "public/assets",
-        assetsDir: "",
+        outDir: 'public/assets',
+        // minify: 'terser',
+        // terserOptions: {
+        //     compress: {
+        //         drop_console: true,
+        //     },
+        // },
+        assetsDir: '',
         rollupOptions: {
-            input: ["resources/main.js", "scripts/toaster.js"],
+            input: ['resources/main.js', 'scripts/toaster.js'],
+            output: {
+                chunkFileNames: 'js/[name]-[hash].js',
+                entryFileNames: 'js/[name]-[hash].js',
+                assetFileNames: '[ext]/[name]-[hash].[ext]',
+            },
         },
+        sourcemap: false,
     },
     css: {
         postcss: {
             plugins: [
                 autoprefixer({
-                    grid: "autoplace",
+                    grid: 'autoplace',
                     // browsers: [
                     // '>1%',
                     // 'last 5 versions',
